f6bb43ebf03f491d23eb8532c9467736ec989eac
Molnár Attila Mátyás <molnarattilamatyas@mailbox.unideb.hu>
2025-11-27 11:36:40 +0100

    Add Movie entity (title, director, year, genre, rating, description, imageUrl)

---
+++ backend/src/main/java/hu/attila/filmajanlo/model/Movie.java
@@ -0,0 +1,28 @@
+package hu.attila.filmajanlo.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+public class Movie {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+
+    private String director;
+
+    private int year;
+
+    private String genre;
+
+    private double rating;
+
+    @Column(length = 2000)
+    private String description;
+
+    private String imageUrl;
+}
